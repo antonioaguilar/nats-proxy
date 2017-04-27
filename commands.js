f550e7c0ca274d482ea82504f159c68be8c37686
Antonio Aguilar <ab6446694@gmail.com>
2017-04-27 17:54:48 +0100

    Release v1.0.0

---
+++ commands.js
@@ -0,0 +1,66 @@
+var pkg = require('./package.json');
+var fs = require('fs-extra');
+
+function getHelp() {
+  console.log([
+    '', 'NATS.io messaging proxy server',
+    '', 'usage: nats-proxy [options]',
+    '',
+    'options:',
+    '  -c --config  Path to configuration file',
+    '  -S --ssl     Enable HTTPS on proxy server',
+    '  -C --cert    Server certificate file',
+    '  -K --key     Private key for server certificate',
+    //'  -T --tls     Enable TLS on NATS server', // disable TLS on NATS because it requires a CA certificate
+    '  -h --help    Print this list and exit',
+    '  -v --version Print the current version',
+    ''
+  ].join('\n'));
+  process.exit();
+}
+
+function getVersion() {
+  console.log(pkg.version);
+  process.exit();
+}
+
+function getConfig(config_file) {
+  try {
+    fs.statSync(config_file).isFile();
+    return fs.readJSONSync(config_file);
+  }
+  catch(err) {
+    console.error('Error in configuration file');
+    process.exit();
+  }
+}
+
+function getCertificate(cert_file) {
+  try {
+    fs.statSync(cert_file).isFile();
+    return fs.readFileSync(cert_file);
+  }
+  catch(err) {
+    console.error('Error in certificate file');
+    process.exit();
+  }
+}
+
+function getKey(cert_key) {
+  try {
+    fs.statSync(cert_key).isFile();
+    return fs.readFileSync(cert_key);
+  }
+  catch(err) {
+    console.error('Error in key file');
+    process.exit();
+  }
+}
+
+module.exports = {
+  getHelp: getHelp,
+  getVersion: getVersion,
+  getConfig: getConfig,
+  getCertificate: getCertificate,
+  getKey: getKey
+};
---
+++ proxy.js
@@ -0,0 +1,113 @@
+#!/usr/bin/env node
+
+var _ = require('lodash');
+var pkg = require('./package.json');
+var cmd = require('./commands');
+var fs = require('fs-extra');
+var NATS = require('nats');
+var http = require('http');
+var https = require('https');
+var express = require('express');
+var monitor = require('express-status-monitor');
+var argv = require('minimist')(process.argv.slice(2));
+var bodyParser = require('body-parser');
+var app = express();
+
+var help = argv.h || argv.help;
+var version = argv.v || argv.version;
+var config_file = argv.c || argv.config;
+var ssl_enabled = argv.S || argv.ssl;
+var nats_tls = argv.T || argv.tls;
+var cert_file = argv.C || argv.cert;
+var key_file = argv.K || argv.key;
+var nats, cert, key, config;
+
+if(help || _.size(argv) === 1) {
+  cmd.getHelp();
+}
+
+if(version) {
+  cmd.getVersion();
+}
+
+if(cert_file) {
+  cert = cmd.getCertificate(cert_file);
+}
+
+if(key_file) {
+  key = cmd.getKey(key_file);
+}
+
+if(config_file) {
+  config = cmd.getConfig(config_file);
+}
+
+var proxy_host = config.proxy.host || '0.0.0.0';
+var proxy_port = config.proxy.port || 4000;
+var nats_servers = config.nats_servers || ['nats://0.0.0.0:4222'];
+var default_route = config.default_route || '/nats-proxy';
+var routes = config.routes || [];
+
+if(!nats_tls) {
+  nats = NATS.connect({ servers: nats_servers });
+}
+else if(nats_tls && cert && key) {
+  var tlsOptions = { key: key, cert: cert };
+  nats = NATS.connect({ servers: nats_servers, tls: tlsOptions });
+}
+else {
+  console.error('Could not configure TLS on NATS server');
+  process.exit();
+}
+
+app.use(monitor());
+app.use(bodyParser.json());
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
+// create the channel routes from config file
+routes.forEach(function(item) {
+  app.post(item.url, function(req, res) {
+    nats.publish(item.channel, JSON.stringify(req.body));
+    res.end();
+  });
+});
+
+// create a default route to publish to a custom channels
+// if "channel_id" is passed on the JSON payload, then we use it as the channel name
+// otherwise we publish any received messages to the DEFAULT channel
+app.post(default_route, function(req, res) {
+  nats.publish(req.body.channel_id || 'DEFAULT', JSON.stringify(req.body));
+  res.end();
+});
+
+if(ssl_enabled && cert && key) {
+  var httpsOptions = { key: key, cert: cert };
+  https.createServer(httpsOptions, app).listen(proxy_port, proxy_host, function() {
+    console.log([
+      '', 'NATS.io messaging proxy server',
+      '',
+      '-  Proxy server running on https://' + proxy_host + ':' + proxy_port,
+      '-  NATS server running on ' + nats.currentServer.url.host,
+      ''
+    ].join('\n'));
+  });
+}
+else {
+  http.createServer(app).listen(proxy_port, proxy_host, function() {
+    console.log([
+      '', 'NATS.io messaging proxy server',
+      '',
+      '-  Proxy server running on http://' + proxy_host + ':' + proxy_port,
+      '-  NATS server running on nats://' + nats.currentServer.url.host,
+      ''
+    ].join('\n'));
+  });
+}
+
+
+
+
